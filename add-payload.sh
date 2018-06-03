0b1aff2d41cefd61a73c2deb48c9d97298328de9
Georg vom Endt <gve@3yourmind.com>
2018-06-03 08:07:10 +0200

    cleaned

--- add-payload.sh
+++ add-payload.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-# Check for payload format option (default is uuencode).
-uuencode=1
-if [[ "$1" == '--binary' ]]; then
-	binary=1
-	uuencode=0
-	shift
+uuencode="on"
+if test "$1" = '--binary'
+then
+  binary="on"
+  uuencode="off"
+  shift
 fi
-if [[ "$1" == '--uuencode' ]]; then
-	binary=0
-	uuencode=1
-	shift
-fi
-
-if [[ ! "$1" ]]; then
-	echo "Usage: $0 [--binary | --uuencode] PAYLOAD_FILE"
-	exit 1
+if test "$1" = '--uuencode'
+then
+  binary="off"
+  uuencode="on"
+  shift
 fi
 
+if test -z "$1"
+then
+  echo "Usage: $0 [--binary | --uuencode] PAYLOAD_FILE"
+  exit 1
+fi
 
-if [[ $binary -ne 0 ]]; then
-	# Append binary data.
-	sed \
-		-e 's/uuencode=./uuencode=0/' \
-		-e 's/binary=./binary=1/' \
+
+if test "$binary" = 'on'
+then
+  sed \
+    -e 's/uuencode=./uuencode=0/' \
+    -e 's/binary=./binary=1/' \
 			 install.sh.in >install.sh
-	echo "PAYLOAD:" >> install.sh
-
+  echo "PAYLOAD:" >> install.sh
 	cat $1 >>install.sh
 fi
-if [[ $uuencode -ne 0 ]]; then
-	# Append uuencoded data.
-	sed \
-		-e 's/uuencode=./uuencode=1/' \
-		-e 's/binary=./binary=0/' \
-			 install.sh.in >install.sh
-	echo "PAYLOAD:" >> install.sh
 
-	cat $1 | uuencode - >>install.sh
+if test $uuencode = 'off'
+then
+  sed \
+    -e 's/uuencode=./uuencode=1/' \
+    -e 's/binary=./binary=0/' \
+        install.sh.in >install.sh
+  echo "PAYLOAD:" >> install.sh
+  cat $1 | uuencode - >>install.sh
 fi
